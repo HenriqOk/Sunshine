acafd82693feca9a89bdd05bc31a1eec8fb8e3f0
Henrique Okomura <henrique.okomura@gmail.com>
2015-02-09 01:46:31 -0200

    Refresh button on forecast fragment added. Created an AsyncTask to fetch the weather data.

--- app/src/main/java/com/studyjam/sunshine/app/MainActivity.java
+++ app/src/main/java/com/studyjam/sunshine/app/MainActivity.java
@@ -1,18 +1,23 @@
 package com.studyjam.sunshine.app;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -25,7 +30,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -53,45 +58,4 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            String[] array = {
-                "Hoje - Sol - 28 / 22",
-               "Amanha - Nublado - 25 / 20",
-               "Terca - Chuva - 22 / 18",
-                "Quarta - Tempestade - 20 / 16",
-                "Quinta - Chuva - 22 / 18",
-                "Sexta - Nublado - 25 / 20",
-                "Quarta - Tempestade - 20 / 16",
-                "Quarta - Tempestade - 20 / 16",
-                "Quarta - Tempestade - 20 / 16",
-                "Quarta - Tempestade - 20 / 16"
-            };
-
-            ArrayList<String> data = new ArrayList(Arrays.asList(array));
-
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                    this.getActivity(),
-                    R.layout.list_item_forecast,
-                    R.id.list_item_forecast_textview,
-                    data
-            );
-
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(adapter);
-
-            return rootView;
-        }
-    }
 }
